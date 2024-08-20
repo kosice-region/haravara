747376409005fdc3a1dfc0a34f1122b4ab5a192c
sinaiko0212 <sinaiovamirka@gmail.com>
2024-08-20 14:02:52 +0200

    scroll bar is changed to look same in ios and android

--- haravara/lib/pages/conditions/widgets/summary_item.dart
+++ haravara/lib/pages/conditions/widgets/summary_item.dart
@@ -28,43 +28,20 @@ class SummaryItem extends StatelessWidget {
         ),
         child: Padding(
           padding: EdgeInsets.all(19.w),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Expanded(
-                child: ScrollbarTheme(
-                  data: ScrollbarThemeData(
-                    thumbColor: MaterialStateProperty.all(
-                      const Color.fromARGB(255, 249, 64, 64),
-                    ),
-                    radius: Radius.circular(10.r),
-                    thickness: MaterialStateProperty.all(8.w),
-                  ),
-                  child: Scrollbar(
-                    thumbVisibility: true,
-                    scrollbarOrientation: ScrollbarOrientation.right,
-                    child: Padding(
-                      padding: EdgeInsets.only(right: 10.w),
-                      child: SingleChildScrollView(
-                        child: Text(
-                          'Pátračka po Kaškovych pečiatkach funguje veľmi jednoducho. Stačí si stiahnuť aplikáciu Haravara Pátračka, zaregistrovať sa pod '
-                          'svojim špeciálnym pátračským menom a môžeš vyraziť na dobrodružnú túru po krajine Haravara! '
-                          'K tomu, aby si nezišiel z cesty, ti bude pomáhať Kaškov kompas, ktorý ťa bude navigovať k presnému miestu, '
-                          'kde môžeš získať jednu z jeho pečiatok. V sekcii "Rebríček" si môžeš sledovať, na akej úrovni zberateľstva '
-                          'sa práve nachádzaš medzi ostatnými pátračmi. Výhra ťa neminie za každých 15 pečiatok! Ak získaš 30 a viac pečiatok, '
-                          'budeš zaradený do žrebovania o skvelý zážitok v kraji!',
-                          style: GoogleFonts.titanOne(
-                            fontSize: 12.sp,
-                            color: Colors.white,
-                          ),
-                          textAlign: TextAlign.center,
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
+          child: SingleChildScrollView(
+            child: Text(
+              'Pátračka po Kaškovych pečiatkach funguje veľmi jednoducho. Stačí si stiahnuť aplikáciu Haravara Pátračka, zaregistrovať sa pod '
+              'svojim špeciálnym pátračským menom a môžeš vyraziť na dobrodružnú túru po krajine Haravara! '
+              'K tomu, aby si nezišiel z cesty, ti bude pomáhať Kaškov kompas, ktorý ťa bude navigovať k presnému miestu, '
+              'kde môžeš získať jednu z jeho pečiatok. V sekcii "Rebríček" si môžeš sledovať, na akej úrovni zberateľstva '
+              'sa práve nachádzaš medzi ostatnými pátračmi. Výhra ťa neminie za každých 15 pečiatok! Ak získaš 30 a viac pečiatok, '
+              'budeš zaradený do žrebovania o skvelý zážitok v kraji!',
+              style: GoogleFonts.titanOne(
+                fontSize: 12.sp,
+                color: Colors.white,
               ),
-            ],
+              textAlign: TextAlign.center,
+            ),
           ),
         ),
       ),
