5a88bffeb9d544faa0b7eaf381d4e289a135e2d2
Erik Mišenčík <erik.misencik@student.tuke.sk>
2024-09-02 11:58:00 +0200

    Preparing branch to PR and Merge to Main

--- haravara/lib/core/widgets/footer.dart
+++ haravara/lib/core/widgets/footer.dart
@@ -4,9 +4,7 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:haravara/pages/auth/models/user.dart';
 import 'package:haravara/pages/profile/providers/avatars.provider.dart';
-import 'package:haravara/pages/profile/providers/user_info_provider.dart'; ///ADDED THIS LINE
 import 'package:haravara/router/router.dart';
 import 'package:haravara/router/screen_router.dart';
 import 'package:haravara/pages/header_menu/view/header_menu_screen.dart';
@@ -28,19 +26,6 @@ class Footer extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-
-    //CODE ONLY FOR TESTING     
-    String userProfileType = ref.watch(userInfoProvider).isFamily ? 'family' : 'individual';
-    String children = ref.watch(userInfoProvider).children.toString();
-    String username = ref.watch(userInfoProvider).username;
-    String userId = ref.watch(userInfoProvider).id;
-    log('Handler of User Data');   
-    log('UserId: ${userId}');
-    log('Username: ${username}');
-    log('User profile type: $userProfileType'); 
-    log('Number of children: $children'); 
-    
-    //END OF CODE ONLY FOR TESTING
 
     imageAssets.forEach((image) => precacheImage(AssetImage(image), context));
     return GestureDetector(
