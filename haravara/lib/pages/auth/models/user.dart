55ad3541c7f10e1bc05333c0e2ce52f88fd612d8
Erik Mišenčík <erik.misencik@student.tuke.sk>
2024-09-03 12:41:57 +0200

    Preparing branch to PR and Merge to Main

--- haravara/lib/pages/auth/models/user.dart
+++ haravara/lib/pages/auth/models/user.dart
@@ -58,7 +58,7 @@ class UserModel {
 
   UserModel(
       {this.isLoggedIn = false,
-      this.isFamily = false,   // changed from true to fasle
+      this.isFamily = false,   
       this.username = '',
       this.location = '',
       this.email = '',
