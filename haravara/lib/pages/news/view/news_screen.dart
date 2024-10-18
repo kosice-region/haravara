49cab2e10eed1f75d3f70a8dac23a38f57a13828
sinaiko0212 <sinaiovamirka@gmail.com>
2024-10-18 21:46:20 +0200

    UI on main screen is changed as in figma + should be responsive

--- haravara/lib/pages/news/view/news_screen.dart
+++ haravara/lib/pages/news/view/news_screen.dart
@@ -2,16 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:haravara/pages/map_detail/providers/places_provider.dart';
 import 'package:haravara/router/router.dart';
-import 'package:haravara/pages/auth/services/auth_service.dart';
 import 'package:haravara/core/services/database_service.dart';
 import 'package:haravara/router/screen_router.dart';
 import 'package:haravara/core/widgets/header.dart';
 import 'package:haravara/pages/header_menu/view/header_menu_screen.dart';
 import 'package:haravara/core/widgets/footer.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-
-import '../../map_detail/map_detail.dart';
 
 class NewsScreen extends ConsumerStatefulWidget {
   const NewsScreen({Key? key}) : super(key: key);
@@ -45,6 +43,7 @@ class _NewsScreenState extends ConsumerState<NewsScreen> {
   @override
   Widget build(BuildContext context) {
     imageAssets.forEach((image) => precacheImage(AssetImage(image), context));
+
     ScreenUtil.init(context, designSize: const Size(255, 516));
 
     return Scaffold(
@@ -70,40 +69,31 @@ class _NewsScreenState extends ConsumerState<NewsScreen> {
             child: Column(
               children: [
                 Header(),
-                20.verticalSpace,
+                10.verticalSpace, 
                 SizedBox(
-                  height: 180.h,
-                  width: 190.w,
-                  child: buildBox(),
-                ),
-                30.verticalSpace,
-                ElevatedButton(
-                  onPressed: () {
-                    routeToNextScreen(context, ScreenType.prizes, ref);
-                  },
-                  style: ElevatedButton.styleFrom(
-                    fixedSize: Size(190.w, 40.h),
-                    backgroundColor: const Color.fromARGB(255, 91, 187, 75),
-                  ),
-                  child: Text(
-                    'VÝHRY',
-                    style: GoogleFonts.titanOne(
-                        fontSize: 12.sp, color: Colors.black),
-                  ),
-                ),
-                const SizedBox(height: 25),
-                ElevatedButton(
-                  onPressed: () {
-                    routeToNextScreen(context, ScreenType.podmienky, ref);
-                  },
-                  style: ElevatedButton.styleFrom(
-                    fixedSize: Size(190.w, 40.h),
-                    backgroundColor: const Color(0xFFFFC944),
-                  ),
-                  child: Text(
-                    'PODMIENKY SÚŤAŽE',
-                    style: GoogleFonts.titanOne(
-                        fontSize: 12.sp, color: Colors.black),
+                  width: double.infinity,
+                  child: Column(
+                    children: [
+                      buildBox(),
+                      SizedBox(height: 10.h), 
+                      Column(
+                        children: [
+                          buildResponsiveButton(
+                            label: 'VÝHRY',
+                            color: const Color.fromARGB(255, 42, 177, 255),
+                            screen: ScreenType.prizes,
+                            ref: ref,
+                          ),
+                          SizedBox(height: 10.h), 
+                          buildResponsiveButton(
+                            label: 'PODMIENKY SÚŤAŽE',
+                            color: const Color.fromARGB(255, 205, 105, 167),
+                            screen: ScreenType.podmienky,
+                            ref: ref,
+                          ),
+                        ],
+                      ),
+                    ],
                   ),
                 ),
               ],
@@ -117,24 +107,18 @@ class _NewsScreenState extends ConsumerState<NewsScreen> {
   Widget buildBox() {
     return Container(
       width: 200.w,
-      padding: EdgeInsets.all(4.w), 
+      height: 210.h,
+      padding: EdgeInsets.all(4.w),
       decoration: BoxDecoration(
         borderRadius: BorderRadius.all(Radius.circular(15)),
-        color: const Color.fromARGB(255, 157, 214, 246),
-        boxShadow: [
-          BoxShadow(
-            color: const Color.fromARGB(255, 157, 214, 246).withOpacity(1),
-            spreadRadius: 8,
-            blurRadius: 8,
-            offset: const Offset(0, 3),
-          ),
-        ],
+        color: const Color.fromARGB(255, 24, 191, 186),
+        border: Border.all(color: Colors.white, width: 4),
       ),
       child: Stack(
-        clipBehavior: Clip.none, 
+        clipBehavior: Clip.none,
         children: [
           Positioned(
-            left: 20.w, 
+            left: 20.w,
             top: 20,
             child: Opacity(
               opacity: 0.3,
@@ -146,13 +130,13 @@ class _NewsScreenState extends ConsumerState<NewsScreen> {
             ),
           ),
           Padding(
-            padding: EdgeInsets.only(left: 5.w, top: 20), 
+            padding: EdgeInsets.only(left: 5.w, top: 20),
             child: Column(
               children: [
                 Text(
                   texts[0],
                   style: GoogleFonts.titanOne(
-                    color: const Color.fromARGB(255, 191, 0, 159),
+                    color: const Color.fromARGB(255, 255, 255, 255),
                     fontSize: 11.sp,
                   ),
                   textAlign: TextAlign.center,
@@ -162,7 +146,7 @@ class _NewsScreenState extends ConsumerState<NewsScreen> {
                 Text(
                   'Zober svojich rodičov a kamarátov na úžasnú cestu po krajine Haravara a získaj všetky Kaškove pečiatky!\nAktuálna sezóna Haravara Pátračky trvá do konca roka 2024!',
                   style: GoogleFonts.titanOne(
-                    color: const Color.fromARGB(255, 191, 0, 159),
+                    color: const Color.fromARGB(255, 255, 255, 255),
                     fontSize: 9.sp,
                   ),
                   textAlign: TextAlign.center,
@@ -171,7 +155,7 @@ class _NewsScreenState extends ConsumerState<NewsScreen> {
             ),
           ),
           Positioned(
-            bottom: 0.h, 
+            bottom: 0.h,
             left: 100,
             right: 0,
             child: Align(
@@ -182,12 +166,13 @@ class _NewsScreenState extends ConsumerState<NewsScreen> {
                 },
                 style: ElevatedButton.styleFrom(
                   fixedSize: Size(120.w, 35.h),
-                  backgroundColor: const Color(0xFFFFC944),
+                  backgroundColor: const Color.fromARGB(255, 239, 72, 77),
+                  side: BorderSide(color: Colors.white, width: 4),
                 ),
                 child: Text(
                   'IDEM PÁTRAŤ',
-                  style:
-                      GoogleFonts.titanOne(fontSize: 9.sp, color: Colors.black),
+                  style: GoogleFonts.titanOne(
+                      fontSize: 9.sp, color: const Color.fromARGB(255, 255, 255, 255)),
                 ),
               ),
             ),
@@ -197,14 +182,29 @@ class _NewsScreenState extends ConsumerState<NewsScreen> {
     );
   }
 
-  Size calculateTextSize(String text, TextStyle style, double maxWidth) {
-    final TextPainter textPainter = TextPainter(
-      text: TextSpan(text: text, style: style),
-      maxLines: null,
-      textDirection: TextDirection.ltr,
-    )..layout(minWidth: 0, maxWidth: maxWidth);
-
-    return textPainter.size;
+  Widget buildResponsiveButton({
+    required String label,
+    required Color color,
+    required ScreenType screen,
+    required WidgetRef ref,
+  }) {
+    return ElevatedButton(
+      onPressed: () {
+        routeToNextScreen(context, screen, ref);
+      },
+      style: ElevatedButton.styleFrom(
+        fixedSize: Size(170.w, 40.h),
+        backgroundColor: color,
+        side: BorderSide(color: Colors.white, width: 4),
+      ),
+      child: Text(
+        label,
+        style: GoogleFonts.titanOne(
+          fontSize: 12.sp,
+          color: const Color.fromARGB(255, 255, 255, 255),
+        ),
+      ),
+    );
   }
 }
 
